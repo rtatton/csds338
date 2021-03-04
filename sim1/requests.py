75f5ad1707798fe2c19a3e9d4adad7aaaddc46a6
Ryan Tatton <rdt17@protonmail.com>
2021-03-04 14:06:39 -0500

    Refactored sampling behavior; updated docstrings
    - allocate() and free() only sample from freed and allocated blocks, respectively.
    - Uses np.random.Generator for all sampling now.
    - Creates global variables for (True, False) and None, which indicates no block was able to be sampled.
    - Renames variables b1, b2 to to_allocate and allocated for readability.
    - Adds "Returns" to most method docstrings.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- sim1/requests.py
+++ sim1/requests.py
@@ -1,9 +1,8 @@
 import io
-import random
 import sys
 from collections import abc
 from typing import (
-	Any, Callable, NoReturn, Optional, Sequence, TextIO, Tuple, Union)
+	Any, Callable, NoReturn, Optional, TextIO, Tuple, Union)
 
 import attr
 import numpy as np
@@ -13,6 +12,8 @@ NoneType = type(None)
 Block = int
 Blocks = Union[Block, Tuple[Block, ...]]
 Result = bool
+TRUE_OR_FALSE = (True, False)
+NO_BLOCK = None
 
 
 # noinspection PyUnresolvedReferences
@@ -23,10 +24,12 @@ class RequestStream(abc.Iterator, abc.Callable):
 	Attributes:
 		blocks: Number of memory blocks.
 		seed: Element to set the random seed.
+		indices: Identifier of each block of memory.
 		pages: Number of pages for each block of memory.
-		allocated: Contains all memory blocks that have been allocated.
+		available: Indicator array that tracks if a block is allocated or free.
 		std_out: Stream to which request results should be written. If None,
 			results will not be written to any stream.
+		_rng: Random generator.
 	"""
 	blocks = attr.ib(
 		type=int, default=100, validator=validators.instance_of(int))
@@ -36,8 +39,9 @@ class RequestStream(abc.Iterator, abc.Callable):
 		default=sys.stdout,
 		validator=validators.instance_of((io.TextIOBase, TextIO, NoneType)),
 		repr=False)
-	pages = attr.ib(type=Union[Sequence[int], np.ndarray], init=False)
-	allocated = attr.ib(type=np.ndarray, init=False)
+	indices = attr.ib(type=np.array, init=False)
+	pages = attr.ib(type=np.ndarray, init=False)
+	available = attr.ib(type=np.ndarray, init=False)
 	_rng = attr.ib(type=np.random.Generator, init=False, repr=False)
 
 	def __attrs_post_init__(self):
@@ -46,9 +50,9 @@ class RequestStream(abc.Iterator, abc.Callable):
 			self._rng = np.random.default_rng()
 		else:
 			self._rng = np.random.default_rng(self.seed)
-			random.seed(self.seed)
+		self.indices = np.arange(self.blocks)
 		self.pages = np.array([self.sample_page() for _ in range(self.blocks)])
-		self.allocated = np.zeros(self.blocks)
+		self.available = np.ones(self.blocks)
 
 	def __call__(self, *args, **kwargs) -> Result:
 		return next(self)
@@ -58,17 +62,26 @@ class RequestStream(abc.Iterator, abc.Callable):
 		_, result = request()
 		return result
 
-	# noinspection PyMethodMayBeStatic
 	def sample_page(self) -> int:
 		"""A discrete probability distribution over memory block pages."""
 		return self._rng.integers(1, 21)
 
 	def sample_request(self) -> Callable[[], Tuple[Blocks, Result]]:
-		"""A discrete probability distribution over request types."""
-		return random.choice((self.allocate, self.me_too, self.free))
+		"""A discrete probability distribution over request types.
+
+		Returns:
+			A request callable.
+		"""
+		if all(self.available):
+			request = self.allocate
+		elif not any(self.available):
+			request = self.free
+		else:
+			request = self._rng.choice((self.allocate, self.free, self.me_too))
+		return request
 
 	def allocate(self) -> Tuple[Block, Result]:
-		"""Samples a block and determines if it should be allocated.
+		"""Samples a free block and determines if it should be allocated.
 
 		Notes:
 			Mathematically, the distribution is defined as p(b), which
@@ -79,27 +92,30 @@ class RequestStream(abc.Iterator, abc.Callable):
 
 		See Also:
 			sample_allocate() - defines the probability distribution.
+
+		Returns:
+			The sampled block and the result if the sampling occurred.
 		"""
-		block = self.sample_block()
-		if self.allocated[block]:
+		if (block := self.sample_block(allocated=False)) is NO_BLOCK:
 			result = False
-		else:
-			result = self.sample_allocate(block)
-			self.allocated[block] = result
+		elif result := self.sample_allocate(block):
+			self.available[block] = False
 		self._print('Allocate', block, result)
 		return block, result
 
-	# noinspection PyMethodMayBeStatic
 	def sample_allocate(self, b: Block) -> Result:
 		"""A discrete, bimodal, unconditional probability distribution.
 
 		See Also:
 			allocate() - implements the remaining sampling logic.
+
+		Returns:
+			True if the sampling should occur and False otherwise.
 		"""
-		return random.choice((True, False))
+		return self._rng.choice(TRUE_OR_FALSE)
 
 	def free(self) -> Tuple[Block, Result]:
-		"""Samples a block and determines if it should be freed.
+		"""Samples an allocated block and determines if it should be freed.
 
 		Notes:
 			Mathematically, the distribution is defined as p(b), which
@@ -110,29 +126,36 @@ class RequestStream(abc.Iterator, abc.Callable):
 
 		See Also:
 			sample_free() - defines the probability distribution.
+
+		Returns:
+			The sampled block and the result if the sampling occurred.
 		"""
-		block = self.sample_block()
-		if self.allocated[block]:
-			result = self.sample_free(block)
-			self.allocated[block] = result
-		else:
+		if (block := self.sample_block(allocated=True)) is NO_BLOCK:
 			result = False
+		elif result := self.sample_free(block):
+			self.available[block] = True
 		self._print('Free', block, result)
 		return block, result
 
-	# noinspection PyMethodMayBeStatic
 	def sample_free(self, b: Block) -> Result:
 		"""A discrete, bimodal, unconditional probability distribution.
 
 		See Also:
 			free() - implements the remaining sampling logic.
+
+		Returns:
+			True if the sampling should occur and False otherwise.
 		"""
-		return random.choice((True, False))
+		return self._rng.choice(TRUE_OR_FALSE)
 
 	def me_too(self) -> Tuple[Tuple[Block, Block], Result]:
 		"""Samples 2 blocks and determines if the first should be allocated.
 
 		Notes:
+			The first (given) block is sampled is allocated, while the second
+			block sampled is free. If either all blocks are allocated or no
+			blocks are free, then the result is always False.
+
 			Mathematically, the distribution is defined as p(b1 | b2), which
 			represents the probability that b1 will be allocated, given b2 is
 			allocated. The first block is never allocated if either (1) it is
@@ -142,33 +165,54 @@ class RequestStream(abc.Iterator, abc.Callable):
 
 		See Also:
 			sample_me_too() - defines the probability distribution.
-		"""
-		b1, b2 = self.sample_block(2)
-		if self.allocated[b2]:
-			if self.allocated[b1]:
-				result = False
-			else:
-				result = self.sample_me_too(b1, b2)
-				self.allocated[b1] = result
-		else:
-			result = False
-		self._print('MeToo', f'{b1}|{b2}', result)
-		return (b1, b2), result
 
-	# noinspection PyMethodMayBeStatic
+		Returns:
+			The 2 sampled blocks and the result if the sampling occurred.
+		"""
+		allocated = self.sample_block(allocated=True)
+		to_allocate = self.sample_block(allocated=False)
+		if allocated is NO_BLOCK or to_allocate is NO_BLOCK:
+			result = False
+		elif result := self.sample_me_too(allocated, to_allocate):
+			self.available[to_allocate] = False
+		self._print('MeToo', f'{to_allocate}|{allocated}', result)
+		return (allocated, to_allocate), result
+
 	def sample_me_too(self, b1: Block, b2: Block) -> Result:
 		"""A discrete, conditional, bimodal probability distribution.
 
 		See Also:
 			me_too() - implements the remaining sampling logic.
+
+		Returns:
+			True if the sampling should occur and False otherwise.
 		"""
-		return random.choice((True, False))
+		return self._rng.choice(TRUE_OR_FALSE)
 
-	def sample_block(self, n: int = 1) -> Blocks:
-		def sample():
-			return self._rng.integers(self.blocks)
+	# noinspection PyIncorrectDocstring
+	def sample_block(self, n: int = 1, *, allocated: bool = True) -> Blocks:
+		"""Samples either allocated or free blocks.
 
-		return sample() if n == 1 else tuple(sample() for _ in range(n))
+		Args:
+			n: Number of blocks to sample. Sampling is done without
+				replacement, so the number of blocks sampled is
+				min(n, pool_size).
+
+		Keyword Args:
+			allocated: True only samples from allocated blocks. False only
+				samples from free blocks.
+
+		Returns:
+			0 or more sampled blocks.
+		"""
+		mask = ~self.available if allocated else self.available
+		pool = np.flatnonzero(mask * self.indices)
+		block = self._rng.choice(pool, size=min(n, pool.size), replace=False)
+		if block.size < 1:
+			block = NO_BLOCK
+		elif n == 1:
+			block = block[0]
+		return block
 
 	def _print(self, request_type: str, params: Any, result: Any) -> NoReturn:
 		if self.std_out:
@@ -179,4 +223,5 @@ if __name__ == '__main__':
 	requests = RequestStream(5)
 	for _ in range(25):
 		requests()
-	print(requests.pages)
+		print(requests.pages)
+		print(requests.available)
