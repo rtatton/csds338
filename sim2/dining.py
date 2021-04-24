fd9ed6c57c23a2f10629ef49b1426c73bc340e93
rdoughty10 <rdoughty20@sjs.org>
2021-04-24 15:49:35 -0400

    Update dining.py

--- sim2/dining.py
+++ sim2/dining.py
@@ -38,31 +38,18 @@ class DiningTable:
 
 	def get_right(self, p: int, return_idx: bool = False) -> bool:
 		"""Check if the chopstick to the right of a philosopher is present."""
-<<<<<<< HEAD
 		idx = (p - 1) % len(self.chopsticks)
 		result = self.chopsticks[idx]
 		return (result, idx) if return_idx else result
 
 	def pick_up(self, p: int, any_chop: bool=False) -> bool:
 		"""Attempts to have the philosopher pick up a chopstick. If successful, changes chopstick[index] to False and Philosopher.right/left to True
-=======
-		if p == 0:
-			idx = 1
-		else:
-			idx = (p + 1) % p
-		result = self.chopsticks[idx]
-		return (result, idx) if return_idx else result
-
-	def pick_up(self, p: int, pr: float = 1) -> bool:
-		"""Attempts to have the philosopher pick up a chopstick.
->>>>>>> 5e85063b077626477f6857b681433669e94b7682
 			Args:
 				p: Index of philosopher picking up the chopstick
 				pr: Probability that the picking up will actually occur
 			Returns:
 				True if the operation succeeded and False otherwise.
 			"""
-<<<<<<< HEAD
 		phil = self.philosophers[p]
 		if not(any_chop):
 			left_on_table, left_idx = self.get_left(p, return_idx=True)
@@ -92,36 +79,12 @@ class DiningTable:
 
 	def put_down(self, p: int) -> bool:
 		"""Attempts to have the philosopher pick up a chopstick. If successful, changes chopstick[index] to True and Philosopher.right/left to False
-=======
-		if result := self._rng.choice((True, False), p=[pr, 1 - pr]):
-			phil = self.philosophers[p]
-			left_on_table, left_idx = self.get_left(p, return_idx=True)
-			right_on_table, right_idx = self.get_right(p, return_idx=True)
-			if result := left_on_table:
-				self.chopsticks[left_idx] = False
-				if phil.state == philosopher.PhilosopherState.WAITING:
-					phil.state = philosopher.PhilosopherState.EATING
-				else:
-					phil.state = philosopher.PhilosopherState.THINKING
-			elif result := right_on_table:
-				self.chopsticks[right_idx] = False
-				if phil.state == philosopher.PhilosopherState.WAITING:
-					phil.state = philosopher.PhilosopherState.EATING
-				else:
-					phil.state = philosopher.PhilosopherState.THINKING
-		return result
-
-	# Todo(rdt17) Update philosopher state?
-	def put_down(self, p: int, pr: float = 1) -> bool:
-		"""Attempts to have the philosopher pick up a chopstick.
->>>>>>> 5e85063b077626477f6857b681433669e94b7682
 			Args:
 				p: Index of philosopher picking up the chopstick
 				pr: Probability that the putting down will actually occur
 			Returns:
 				True if the operation succeeded and False otherwise.
 		"""
-<<<<<<< HEAD
 		phil = self.philosophers[p]
 		if result := phil.left_chop >= 0:
 			self.chopsticks[phil.left_chop] = True
@@ -131,13 +94,3 @@ class DiningTable:
 			phil.right_chop = -1
 		return result
 			
-=======
-		if result := self._rng.choice((True, False), p=[pr, 1 - pr]):
-			left_on_table, left_idx = self.get_left(p, return_idx=True)
-			right_on_table, right_idx = self.get_right(p, return_idx=True)
-			if result := not left_on_table:
-				self.chopsticks[left_idx] = True
-			elif result := not right_on_table:
-				self.chopsticks[right_idx] = False
-		return result
->>>>>>> 5e85063b077626477f6857b681433669e94b7682
