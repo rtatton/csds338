125b2fbfd9cd9a91d04074dd43d1f894ce20548f
Ryan Tatton <rdt17@protonmail.com>
2021-04-24 20:04:40 -0400

    Formats metrics.py
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- sim2/metrics.py
+++ sim2/metrics.py
@@ -1,39 +1,52 @@
 import attr
+import numpy as np
 from attr import validators
+
 import dining
 from philosopher import PhilosopherState
 
 
-
 @attr.s(slots=True)
 class Metrics:
-    '''
-    Attributes:
-        dp: Dining Philosophers instance to track as it runs through simulation
-        deadlocks: int amount of times the program experienced deadlock
-        total_wait_time: int amount of time philosophers were waiting to get a second chopstick
-    '''
-    dp = attr.ib(type=dining.DiningTable, validator=validators.instance_of(dining.DiningTable))
-    deadlocks = attr.ib(type=int, default=0, validator=validators.instance_of(int))
-    total_wait_time = attr.ib(type=int, default=0, validator=validators.instance_of(int))
+	"""
+	Attributes:
+		dp: Dining Philosophers instance to track as it runs through simulation
+		deadlocks: int amount of times the program experienced deadlock
+		total_wait_time: int amount of time philosophers were waiting to get
+			a second chopstick
+	"""
+	dp = attr.ib(
+		type=dining.DiningTable,
+		validator=validators.instance_of(dining.DiningTable))
+	deadlocks = attr.ib(
+		type=int, default=0,
+		validator=validators.instance_of(int))
+	total_wait_time = attr.ib(
+		type=int, default=0,
+		validator=validators.instance_of(int))
 
-    def run_metrics(self, dp):
-        """Basic call that updates all metrics after every time step of a simulation"""
-        self.update_dp(dp)
+	def run_metrics(self, dp):
+		"""Updates all metrics after every time step of a simulation"""
+		self.update_dp(dp)
 
-    def update_dp(self, dp):
-        """updates the dp instance in the metrics"""
-        self.dp = dp
-    
-    def increase_deadlock(self):
-        """adds to total time in deadlock if dining philosophers are in deadlock"""
-        self.deadlocks += 1
+	def update_dp(self, dp):
+		"""Updates the dp instance in the metrics"""
+		self.dp = dp
 
-    def get_total_wait_time(self):
-        for philosopher in self.dp.philosophers:
-            if philosopher.state == PhilosopherState.WAITING:
-                self.total_wait_time += 1
-    
-    def print_metrics(self):
-        print("Total Deadlocks:", self.deadlocks)
+	def increase_deadlock(self):
+		"""Adds to total time in deadlock if philosophers are in deadlock"""
+		self.deadlocks += 1
 
+	def get_total_wait_time(self):
+		for philosopher in self.dp.philosophers:
+			if philosopher.state == PhilosopherState.WAITING:
+				self.total_wait_time += 1
+
+	def print_metrics(self):
+		print("Total Deadlocks:", self.deadlocks)
+
+	def num_deadlocks(self, result: np.ndarray):
+		pass
+
+	def state_stats(self, result: np.ndarray, state: PhilosopherState):
+		pass
